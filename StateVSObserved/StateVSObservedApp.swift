d92de3a1e147040d2835435cea7409d3e526a0f0
Serhii Kyrylenko <32450732+Devepre@users.noreply.github.com>
2021-10-13 15:07:15 +0300

    Initial Commit

---
+++ StateVSObserved/StateVSObservedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateVSObservedApp.swift
+//  StateVSObserved
+//
+//  Created by Serhii Kyrylenko on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateVSObservedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
